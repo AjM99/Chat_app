4d8371a1a2742db856ae2f5c6ccf1301455fbb99
Anuj MOre <anuj.more10424@gmail.com>
2021-04-04 19:12:21 +0530

    1

---
+++ lib/refactor.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'Constants.dart';
+
+class MyTextField extends StatelessWidget {
+  MyTextField({this.icon, this.string});
+  final IconData icon;
+  final String string;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onChanged: (value) {
+        //Do something with the user input.
+      },
+      decoration: InputDecoration(
+        hintText: string,
+        filled: true,
+        fillColor: Colors.blueGrey[500],
+        contentPadding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.blueGrey, width: 1.0),
+          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.blueGrey[800], width: 2.0),
+          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+        ),
+        prefixIcon: Icon(icon),
+      ),
+    );
+  }
+}
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:chat_app/refactor.dart';
+
+class LoginScreen extends StatefulWidget {
+  static String id = "login_screen";
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(
+              FontAwesomeIcons.comments,
+              size: 100,
+              color: Colors.blueGrey,
+            ),
+            SizedBox(height: 48.0),
+            MyTextField(
+              icon: FontAwesomeIcons.envelope,
+              string: "Enter your mail",
+            ),
+            SizedBox(height: 8.0),
+            MyTextField(
+              icon: FontAwesomeIcons.unlockAlt,
+              string: "Enter your password",
+            ),
+            SizedBox(
+              height: 24.0,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                color: Colors.lightBlueAccent,
+                borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                elevation: 5.0,
+                child: MaterialButton(
+                  onPressed: () {
+                    //Implement login functionality.
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: Text(
+                    'Log In',
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
